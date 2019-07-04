f29100a565a348585a736e275a110d2726672353
DIPAK AHER <aherdipak3@gmail.com>
2019-07-04 23:46:44 +0530

    ModelAndView in spring boot

--- Part-1/MyWebApp/src/main/java/com/dac/demo/HomeController.java
+++ Part-1/MyWebApp/src/main/java/com/dac/demo/HomeController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -35,6 +37,37 @@ public class HomeController {
 		2. session.setAttribute("name", name); - we can use java code on jsp page and get session value or we can use JSTL/EL */
 		
 		return "home";
+	}
+	
+	//
+	// ModelAndView
+	//
+	/*@RequestMapping("/home2")
+	public String homePage2(String name,HttpSession session ) {
+		
+		System.out.println("name : "+ name);
+		session.setAttribute("name", name);
+		
+		return "home";
+	}*/
+	
+	/*@RequestMapping("/home2")
+	public String homePage2(@RequestParam("name")String myName,HttpSession session ) {
+		
+		System.out.println("name : "+ myName);
+		session.setAttribute("name", myName);
+		
+		return "home";
+	}*/
+	
+	@RequestMapping("/home2")
+	public ModelAndView homePage2(@RequestParam("name")String myName) {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		
+		return mv;
 	}
 	
 }
