e74eda421b9ef731b89698391ceac97e636cbe31
DIPAK AHER <aherdipak3@gmail.com>
2019-07-05 23:26:47 +0530

    Bean

---
+++ Part-1/MyWebApp/src/main/java/com/dac/demo/Student.java
@@ -0,0 +1,32 @@
+package com.dac.demo;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private String lang;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLang() {
+		return lang;
+	}
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	
+	
+	
+}
