97a270b36f7bef8d54ea3b581797284e239a8563
DIPAK AHER <aherdipak3@gmail.com>
2019-07-11 23:05:22 +0530

    Delete & Put request

--- Part-1/SpringBoot_Jpa/src/main/java/com/dac/starter/controller/StudentController.java
+++ Part-1/SpringBoot_Jpa/src/main/java/com/dac/starter/controller/StudentController.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -128,7 +130,19 @@ public class StudentController {
 		return student;
 	}
 	
+	//********** Put *************
+	@PutMapping(path="/add",consumes = {"application/json"})
+	public Student saveOrUpdateStudent(@RequestBody Student student) {
+		stdRepo.save(student);
+		return student;
+	}
 	
-	
+	//********** DELETE *************
+	@DeleteMapping("/student/{stdId}")
+	public boolean delete(@PathVariable("stdId") int stdId) {
+		Student s = stdDao.getOne(stdId);
+		stdDao.delete(s);
+		return true;
+	}
 	
 }
