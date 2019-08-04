f720ae10b05417484e18628903b4e28e45411ec9
DIPAK AHER <aherdipak3@gmail.com>
2019-08-04 16:23:01 +0530

    @Query annoatation in Spring data JPA with Native Query(SQL query)

--- Part-2/SpringBoot_CRUD_CrudRepository_JPA_MYSQL/src/main/java/com/dac/app/dao/QueryAnnotationDao.java
+++ Part-2/SpringBoot_CRUD_CrudRepository_JPA_MYSQL/src/main/java/com/dac/app/dao/QueryAnnotationDao.java
@@ -10,10 +10,12 @@ import com.dac.app.bean.Student;
 
 public interface QueryAnnotationDao extends JpaRepository<Student, Integer> {
 
-	@Query(value = "SELECT s FROM Student s WHERE s.firstName = ?1")
+	//@Query(value = "SELECT s FROM Student s WHERE s.firstName = ?1")
+	@Query(value = "SELECT * FROM STUDENT FIRST_NAME = ?1",nativeQuery = true)
 	List<Student> getStudentInfoByName(String firstName);
 
-	@Query(value = "SELECT s FROM Student s WHERE s.id = ?1 and s.firstName = ?2")
+	//@Query(value = "SELECT s FROM Student s WHERE s.id = ?1 and s.firstName = ?2")
+	@Query(value = "SELECT * FROM STUDENT  WHERE ID = ?1 AND FIRST_NAME = ?2",nativeQuery = true)
 	Student getStudentInfoByIdAndFirstName(int id, String firstName);
 
 }
